7969317f443ed7124515986ca4984e6040b18ade
rayj1993 <418411718@qq.com>
2019-09-21 19:44:38 +0800

    添加es6模块 css12代码修改

---
+++ config.js
@@ -0,0 +1,49 @@
+// 页面单元
+let unit = {
+    css: [
+        '左右对称布局',
+        'tooltip',
+        'login',
+        'chatroom',
+        'grid',
+        'player',
+        '上下布局',
+        '等宽布局',
+        '左中右',
+        'navbar',
+        'cell',
+        'absulute'
+    ],
+    dom: [
+        'backtop',
+        'domSelect',
+        'closestAll',
+        'slider',
+        'querySelectorAll',
+        'longclick',
+        'linear-gradient',
+        'getAttribute',
+        'loginform',
+        'textarea',
+        'dialog',
+    ],
+    js: [
+        'regex规则表达式',
+        '数组位置调换',
+        'equal',
+        'content长度规则表达式',
+        '种子随机数',
+        'cookie',
+        '__toFixed',
+        '数组操作',
+        'encodeURI',
+        'XMLHttpRequest',
+        '还是正则操作'
+    ],
+    es5:[
+
+    ],
+    es6:[
+        'class'
+    ]
+};
